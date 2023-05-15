2ef19e65107af591402b5923cfe05fefe34bae0f
Tommaso Falchi <tommaso.falchi@mamacrowd.com>
2023-05-15 15:32:06 +0200

    test revision

--- __tests__/core.test.js
+++ __tests__/core.test.js
@@ -5,11 +5,11 @@ const cp = require('child_process')
 jest.setTimeout(180000)
 
 test('Test core errors', () => {
-  process.env['GITHUB_WORKSPACE'] = '.'
-  const ip = path.join(__dirname, '..', 'index.js')
   const options = {
     env: process.env
   }
+  options.env['GITHUB_WORKSPACE'] = '.'
+  const ip = path.join(__dirname, '..', 'index.js')
   try {
     cp.execSync(`node ${ip}`, options).toString()
     fail('Should not succeed, because values miss')
@@ -17,16 +17,16 @@ test('Test core errors', () => {
     expect(error).toBeDefined()
   }
   try {
-    process.env['GITHUB_WORKSPACE'] = '.'
-    process.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, 'core.test.js')
+    options.env['GITHUB_WORKSPACE'] = '.'
+    options.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, 'core.test.js')
     cp.execSync(`node ${ip}`, options).toString()
     fail('Should not succeed, because no valid json')
   } catch (error) {
     expect(error).toBeDefined()
   }
   try {
-    process.env['GITHUB_WORKSPACE'] = '.'
-    process.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, '..', 'package.json')
+    options.env['GITHUB_WORKSPACE'] = '.'
+    options.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, '..', 'package.json')
     cp.execSync(`node ${ip}`, options).toString()
     fail('Should not succeed, because no valid report')
   } catch (error) {
@@ -35,12 +35,12 @@ test('Test core errors', () => {
 })
 
 test('Test core minimal functionality', () => {
-  process.env['GITHUB_WORKSPACE'] = '.'
-  process.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, 'coverage-summary.json')
-  const ip = path.join(__dirname, '..', 'index.js')
   const options = {
     env: process.env
   }
+  options.env['GITHUB_WORKSPACE'] = '.'
+  options.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, 'coverage-summary.json')
+  const ip = path.join(__dirname, '..', 'index.js')
   cp.execSync(`node ${ip}`, options).toString()
   const result = cp.execSync(`node ${ip}`, options).toString()
   for (const value of ['lines::', 47, 37, 0, 78.72]) {
@@ -55,23 +55,23 @@ test('Test core minimal functionality', () => {
 })
 
 test('Test core threashold and minimal custom templates functionality', () => {
-  process.env['GITHUB_WORKSPACE'] = '.'
-  process.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, 'coverage-summary.json')
-  process.env['INPUT_WARNING_THRESHOLD'] = 70
-  process.env['INPUT_LINES_TOTAL_TEMPLATE'] = '{{total}},{{covered}},{{skipped}},{{pct}}'
-  process.env['INPUT_LINES_BADGE_TEMPLATE'] = '{{color}}'
-  const ip = path.join(__dirname, '..', 'index.js')
   const options = {
     env: process.env
   }
+  options.env['GITHUB_WORKSPACE'] = '.'
+  options.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, 'coverage-summary.json')
+  options.env['INPUT_WARNING_THRESHOLD'] = 70
+  options.env['INPUT_LINES_TOTAL_TEMPLATE'] = '{{total}},{{covered}},{{skipped}},{{pct}}'
+  options.env['INPUT_LINES_BADGE_TEMPLATE'] = '{{color}}'
+  const ip = path.join(__dirname, '..', 'index.js')
   let result = cp.execSync(`node ${ip}`, options).toString()
   for (const value of ['lines_badge::', 'yellow']) {
     expect(result).toContain(String(value))
   }
 
-  process.env['INPUT_WARNING_THRESHOLD'] = 60
-  process.env['INPUT_GOOD_THRESHOLD'] = 70
-  result = cp.execSync(`node ${ip}`, {env: process.env}).toString()
+  options.env['INPUT_WARNING_THRESHOLD'] = 60
+  options.env['INPUT_GOOD_THRESHOLD'] = 70
+  result = cp.execSync(`node ${ip}`, options).toString()
   for (const value of ['lines_badge::', 'brightgreen']) {
     expect(result).toContain(String(value))
   }
@@ -81,16 +81,16 @@ test('Test core threashold and minimal custom templates functionality', () => {
 })
 
 test('Test core custom templates functionality and fail if threshold', () => {
-  process.env['GITHUB_WORKSPACE'] = '.'
-  process.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, 'coverage-summary.json')
-  process.env['INPUT_WARNING_THRESHOLD'] = 60
-  process.env['INPUT_GOOD_THRESHOLD'] = 90
-  process.env['INPUT_LINES_TOTAL_TEMPLATE'] = '{{total}},{{covered}},{{skipped}},{{pct}}'
-  process.env['INPUT_LINES_BADGE_TEMPLATE'] = '{{color}}'
-  const ip = path.join(__dirname, '..', 'index.js')
   const options = {
     env: process.env
   }
+  options.env['GITHUB_WORKSPACE'] = '.'
+  options.env['INPUT_SUMMARY_FILE'] = path.join(__dirname, 'coverage-summary.json')
+  options.env['INPUT_WARNING_THRESHOLD'] = 60
+  options.env['INPUT_GOOD_THRESHOLD'] = 90
+  options.env['INPUT_LINES_TOTAL_TEMPLATE'] = '{{total}},{{covered}},{{skipped}},{{pct}}'
+  options.env['INPUT_LINES_BADGE_TEMPLATE'] = '{{color}}'
+  const ip = path.join(__dirname, '..', 'index.js')
   let result = cp.execSync(`node ${ip}`, options).toString()
   expect(result.split('\n')).toEqual(['', '::set-output name=lines::47,37,0,78.72', '', '::set-output name=lines_badge::yellow', ''])
   process.env['INPUT_FAIL_IF'] = 'true'
